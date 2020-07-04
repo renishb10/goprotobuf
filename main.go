843c8399f450b7466e70cb1a04fab3f9b09f8084
Renish B <renishb10@gmail.com>
2020-07-05 00:35:44 +0530

    added JSON to proto viceversa

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"io/ioutil"
 	"log"
 
+	"github.com/golang/protobuf/jsonpb"
 	"github.com/golang/protobuf/proto"
 	simplepb "github.com/renishb10/goprotobuf/src/simple"
 )
@@ -13,16 +14,11 @@ func main() {
 	fmt.Println("Hello World")
 	sm := getSimplePB()
 
-	//Write TO File
-	writeToFile("simplepbfile.bin", sm)
+	// read and write from file
+	readWriteFile(sm)
 
-	result := &simplepb.Simple{}
-
-	//Read from File
-	readFromFile("simplepbfile.bin", result)
-
-	fmt.Println("Read successfully -> ", result)
-
+	// read and write JSON
+	readWriteJson(sm)
 }
 
 func getSimplePB() *simplepb.Simple {
@@ -32,6 +28,18 @@ func getSimplePB() *simplepb.Simple {
 		Age:       31,
 	}
 	return &s
+}
+
+func readWriteFile(sm proto.Message) {
+	//Write TO File
+	writeToFile("simplepbfile.bin", sm)
+
+	result := &simplepb.Simple{}
+
+	//Read from File
+	readFromFile("simplepbfile.bin", result)
+
+	fmt.Println("Read successfully -> ", result)
 }
 
 func writeToFile(fName string, pb proto.Message) error {
@@ -64,4 +72,36 @@ func readFromFile(fName string, pb proto.Message) error {
 	}
 
 	return nil
+}
+
+func pbToJSONString(pb proto.Message) string {
+	marshaler := jsonpb.Marshaler{}
+	out, err := marshaler.MarshalToString(pb)
+	if err != nil {
+		log.Fatalln("Cant convert to JSON", err)
+	}
+	return out
+}
+
+func jSONToPbMessage(in string, pb proto.Message) error {
+	err := jsonpb.UnmarshalString(in, pb)
+	if err != nil {
+		log.Fatalln("Cant convert JSON to message", err)
+		return err
+	}
+	return nil
+}
+
+func readWriteJson(sm proto.Message) {
+	// to JSON string
+	js := pbToJSONString(sm)
+	fmt.Println(js)
+
+	// to Pb Message
+	sb := &simplepb.Simple{}
+	err := jSONToPbMessage(js, sb)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(sb)
 }
